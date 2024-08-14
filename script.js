a54dc07fa17023e7f18b9a52627c8b8a559ea528
Abhi00045 <128639147+Abhi00045@users.noreply.github.com>
2024-08-14 19:31:04 +0530

    doing

---
+++ script.js
@@ -0,0 +1,10 @@
+let burgerCheck = document.getElementById("burgerCheck");
+let orderNow = document.getElementById("orderNow");
+let imageGenerator = document.getElementById("imgGenerator");
+
+orderNow.addEventListener("click",function(){
+    if(burgerCheck.value == true){
+        console.log(`hi`);
+        
+    }
+})
