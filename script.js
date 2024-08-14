b3f996aa25365fa330a49e798fe5cdc68948f419
Abhi00045 <128639147+Abhi00045@users.noreply.github.com>
2024-08-15 01:17:26 +0530

    doing

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const checkBoxes = document.querySelectorAll("#burgerCheck");
-console.log(checkBoxes);
+// console.log(checkBoxes);
 
 let orderNow = document.getElementById("orderNow");
 let imageGenerator = document.getElementById("imgGenerator");
+let imgTag = document.getElementById("imgTag");
 let arr =[];
 
 orderNow.addEventListener("click",function(){
@@ -11,11 +12,25 @@ orderNow.addEventListener("click",function(){
         if(checkbox.checked){
             arr.push(checkbox.value);
         }
+        // console.log(arr);
+        
       });
     //   console.log(arr);
     if(arr.length === 0){
         alert("Pick at least one item");
+        return;
     }
+    const randomFood = arr[Math.floor(Math.random() * arr.length)];
+    let foodImage = document.createElement("img");
+    // console.log(randomFood);
+    if (randomFood === 'Burger') {
 
-      
+      } else if (randomFood === 'Fries') {
+        foodImage.src = 'https://source.unsplash.com/random/600x400?healthy-fries';
+      } else if (randomFood === 'Drink') {
+        foodImage.src = 'https://source.unsplash.com/random/600x400?drink';
+      } 
+    
+    
+    
 })
